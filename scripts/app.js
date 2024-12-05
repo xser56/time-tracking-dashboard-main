9861eebd1580f7703e12e09ea3c3bd6b552db5a1
xser56 <gettosheep56@gmail.com>
2024-12-05 09:56:06 -0800

    daily button work

--- scripts/app.js
+++ scripts/app.js
@@ -1,30 +1,53 @@
-
-// Buttons
+// Buttons Ids
 let daily = document.getElementById("daily");
 let weekly = document.getElementById("weekly");
 let monthly = document.getElementById("monthly");
 
-// Changes
-let currentTime = document.getElementById("currentTime");
-let previousTime = document.getElementById("previosTime");
+// Changed Ids
+let workCurrent = document.getElementById("workCurrent");
+let workPrevious = document.getElementById("workPrevious");
 
+let playCurrent = document.getElementById("playCurrent");
+let playPrevious = document.getElementById("playPrevious");
+
+let studyCurrent = document.getElementById("playCurrent");
+let studyPrevious = document.getElementById("playPrevious");
+
+let exerciseCurrent = document.getElementById("playCurrent");
+let exercisePrevious = document.getElementById("playPrevious");
+
+let socialCurrent = document.getElementById("socialCurrent");
+let socialPrevious = document.getElementById("socialPrevious");
+
+let selfcareCurrent = document.getElementById("selfcareCurrent");
+let selfcarePrevious = document.getElementById("selfcarePrevious");
+
+// Variables
 let timeList = [];
 
 // Functions
-async function apiCall() 
+async function getTimes() 
 {
     const response = await fetch("../data/data.json");
     const data = await response.json();
+    console.log(data)
     
     console.log(data[0].timeframes.daily.current);
-    const dailyCurrent = data[0].timeframes.daily.current;
-
-    const output = document.getElementById("output");
-    output.textContent = `Daily Current Value: ${dailyCurrent}`;
+    return data;
+    
 };
-apiCall()
+getTimes()
 
-daily.addEventListener('click', function(event)
-{
-    currentTime.innerHTML = apiCall(data[0].timeframes.daily.current);  
+// daily.addEventListener('click', function(event)
+// {
+//     workCurrent.innerText = "test";
+// });
+
+daily.addEventListener('click', async function (event) {
+    const data = await getTimes(); 
+    workCurrent.innerText = `${data[0].timeframes.daily.current} hrs`; 
+
+    workPrevious.innerText = `Last Week - ${data[0].timeframes.daily.previous} hrs`;
 });
+
+
