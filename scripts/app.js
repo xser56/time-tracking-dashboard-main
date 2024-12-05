1167e3bb9228adbe3fb64dac60420c29ddb4762f
xser56 <gettosheep56@gmail.com>
2024-12-05 10:39:16 -0800

    js Done

--- scripts/app.js
+++ scripts/app.js
@@ -10,11 +10,11 @@ let workPrevious = document.getElementById("workPrevious");
 let playCurrent = document.getElementById("playCurrent");
 let playPrevious = document.getElementById("playPrevious");
 
-let studyCurrent = document.getElementById("playCurrent");
-let studyPrevious = document.getElementById("playPrevious");
+let studyCurrent = document.getElementById("studyCurrent");
+let studyPrevious = document.getElementById("studyPrevious");
 
-let exerciseCurrent = document.getElementById("playCurrent");
-let exercisePrevious = document.getElementById("playPrevious");
+let exerciseCurrent = document.getElementById("exerciseCurrent");
+let exercisePrevious = document.getElementById("exercisePrevious");
 
 let socialCurrent = document.getElementById("socialCurrent");
 let socialPrevious = document.getElementById("socialPrevious");
@@ -22,8 +22,6 @@ let socialPrevious = document.getElementById("socialPrevious");
 let selfcareCurrent = document.getElementById("selfcareCurrent");
 let selfcarePrevious = document.getElementById("selfcarePrevious");
 
-// Variables
-let timeList = [];
 
 // Functions
 async function getTimes() 
@@ -38,16 +36,70 @@ async function getTimes()
 };
 getTimes()
 
-// daily.addEventListener('click', function(event)
-// {
-//     workCurrent.innerText = "test";
-// });
-
-daily.addEventListener('click', async function (event) {
+daily.addEventListener('click', async function (event)
+{
     const data = await getTimes(); 
-    workCurrent.innerText = `${data[0].timeframes.daily.current} hrs`; 
 
+    workCurrent.innerText = `${data[0].timeframes.daily.current} hrs`; 
     workPrevious.innerText = `Last Week - ${data[0].timeframes.daily.previous} hrs`;
+
+    playCurrent.innerText = `${data[1].timeframes.daily.current} hrs`; 
+    playPrevious.innerText = `Last Week - ${data[1].timeframes.daily.previous} hrs`;
+
+    studyCurrent.innerText = `${data[2].timeframes.daily.current} hrs`; 
+    studyPrevious.innerText = `Last Week - ${data[2].timeframes.daily.previous} hrs`;
+
+    exerciseCurrent.innerText = `${data[3].timeframes.daily.current} hrs`; 
+    exercisePrevious.innerText = `Last Week - ${data[3].timeframes.daily.previous} hrs`;
+
+    socialCurrent.innerText = `${data[4].timeframes.daily.current} hrs`; 
+    socialPrevious.innerText = `Last Week - ${data[4].timeframes.daily.previous} hrs`;
+    
+    selfcareCurrent.innerText = `${data[5].timeframes.daily.current} hrs`; 
+    selfcarePrevious.innerText = `Last Week - ${data[5].timeframes.daily.previous} hrs`;
 });
 
+weekly.addEventListener('click', async function (event)
+{
+    const data = await getTimes();
 
+    workCurrent.innerText = `${data[0].timeframes.weekly.current} hrs`;
+    workPrevious.innerText = `Last Week - ${data[0].timeframes.weekly.previous} hrs`;
+
+    playCurrent.innerText = `${data[1].timeframes.weekly.current} hrs`; 
+    playPrevious.innerText = `Last Week - ${data[1].timeframes.weekly.previous} hrs`;
+
+    studyCurrent.innerText = `${data[2].timeframes.weekly.current} hrs`; 
+    studyPrevious.innerText = `Last Week - ${data[2].timeframes.weekly.previous} hrs`;
+
+    exerciseCurrent.innerText = `${data[3].timeframes.weekly.current} hrs`; 
+    exercisePrevious.innerText = `Last Week - ${data[3].timeframes.weekly.previous} hrs`;
+
+    socialCurrent.innerText = `${data[4].timeframes.weekly.current} hrs`; 
+    socialPrevious.innerText = `Last Week - ${data[4].timeframes.weekly.previous} hrs`;
+
+    selfcareCurrent.innerText = `${data[5].timeframes.weekly.current} hrs`; 
+    selfcarePrevious.innerText = `Last Week - ${data[5].timeframes.weekly.previous} hrs`;
+});
+
+monthly.addEventListener('click', async function (event)
+{
+    const data = await getTimes();
+    workCurrent.innerText = `${data[0].timeframes.monthly.current} hrs`;
+    workPrevious.innerText = `Last Week - ${data[0].timeframes.monthly.previous} hrs`;
+
+    playCurrent.innerText = `${data[1].timeframes.monthly.current} hrs`; 
+    playPrevious.innerText = `Last Week - ${data[1].timeframes.monthly.previous} hrs`;
+
+    studyCurrent.innerText = `${data[2].timeframes.monthly.current} hrs`; 
+    studyPrevious.innerText = `Last Week - ${data[2].timeframes.monthly.previous} hrs`;
+
+    exerciseCurrent.innerText = `${data[3].timeframes.monthly.current} hrs`; 
+    exercisePrevious.innerText = `Last Week - ${data[3].timeframes.monthly.previous} hrs`;
+
+    socialCurrent.innerText = `${data[4].timeframes.monthly.current} hrs`; 
+    socialPrevious.innerText = `Last Week - ${data[4].timeframes.monthly.previous} hrs`;
+
+    selfcareCurrent.innerText = `${data[5].timeframes.monthly.current} hrs`; 
+    selfcarePrevious.innerText = `Last Week - ${data[5].timeframes.monthly.previous} hrs`;
+});
